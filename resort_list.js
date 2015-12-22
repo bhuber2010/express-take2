adf181747d0e211f268fac0c24b1b1efa620b45d
bhuber2010 <bhuber@alumni.colostate.edu>
2015-12-22 11:09:18 -0700

    CRUD warmup done

---
+++ resort_list.js
@@ -0,0 +1,34 @@
+resorts = [
+  {
+    id: "1",
+    name: "Vail",
+    location: "Vail, CO",
+    rating: "5"
+  },
+  {
+    id: "2",
+    name: "Breckenridge",
+    location: "Breckenridge, CO",
+    rating: "5"
+  },
+  {
+    id: "3",
+    name: "Beaver Creek",
+    location: "Avon, CO",
+    rating: "5"
+  },
+  {
+    id: "7",
+    name: "Keystone",
+    location: "Summit County, CO",
+    rating: "4"
+  },
+  {
+    id: "4",
+    name: "Hevenly",
+    location: "Tahoe, CO",
+    rating: "4"
+  },
+]
+
+module.exports = resorts;
